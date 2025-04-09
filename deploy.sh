ea95d6123e6a45dccd9ff642ecbb93732455f862
Khondoker Md Mamunur Rashid <charlicoder@gmail.com>
2025-04-09 23:33:46 +0300

    updated create fusion card tools

--- deploy.sh
+++ deploy.sh
@@ -8,18 +8,31 @@ log_message() {
     echo "$(date +"%Y-%m-%d %H:%M:%S") - $1" | tee -a $LOG_FILE
 }
 
-log_message "Starting deployment of LinkFusionAIAgent..."
+log_message "Starting deployment of aiagent..."
 
 # Pull the latest changes from the main branch
 log_message "Pulling latest changes from Git..."
-git pull origin main || { log_message "Failed to pull from Git. Exiting."; exit 1; }
+git pull origin develop || { log_message "Failed to pull from Git. Exiting."; exit 1; }
 
-# Restart FastAPI service
-log_message "Restarting FastAPI service..."
-sudo systemctl restart fastapi.service || { log_message "Failed to restart FastAPI. Exiting."; exit 1; }
+echo "creating docker image"
+docker build -t aiagent  .
 
-# Restart Nginx service
-log_message "Restarting Nginx service..."
-sudo systemctl restart nginx.service || { log_message "Failed to restart Nginx. Exiting."; exit 1; }
+### Complete Deployment
+echo "killing the running docker"
+docker ps -a | egrep 'aiagent' | awk '{print $1}'| xargs docker kill
+docker ps -a | egrep 'aiagent' | awk '{print $1}'| xargs docker rm
 
-log_message "Deployment completed successfully!"
+
+# echo "running celery"
+# celery -A core worker -l info -D
+
+echo "running the aiagent using docker"
+
+docker run -d \
+    --restart=unless-stopped \
+    --name aiagent \
+    -p 8002:8002 \
+    -v /home/linkfusion/production/linkfusionaiagent/data:/app/media \
+    aiagent
+
+echo "We are done !"
